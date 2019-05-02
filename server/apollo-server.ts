e4ff3bdddb54c8f84f3261a6c22080d006274700
grimmer <grimmer0125@gmail.com>
2019-05-02 14:35:50 +0800

    Add a pure apollo server version without explict apollo-server-express

---
+++ server/apollo-server.ts
@@ -0,0 +1,67 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const { PubSub } = require("apollo-server");
+const POST_ADDED = "POST_ADDED";
+const pubsub = new PubSub();
+
+// const GRAPHQL_ENDPOINT = "http://localhost:4000/graphql";
+// const GRAPHQL_WS_ENDPOINT = "ws://localhost:4000/subscriptions";
+
+const typeDefs = gql`
+  type Subscription {
+    postAdded: Post
+  }
+
+  type Post {
+    author: String
+    comment: String
+  }
+
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Subscription: {
+    postAdded: {
+      // Additional event labels can be passed to asyncIterator creation
+      subscribe: () => pubsub.asyncIterator([POST_ADDED])
+    }
+  },
+  Query: {
+    hello: () => {
+      console.log("get hello query");
+      setTimeout(() => {
+        console.log("publish post_add");
+        pubsub.publish(POST_ADDED, {
+          postAdded: { author: "grimmer", comment: "11" }
+        });
+      }, 5000);
+      return "Hello world!";
+    }
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  subscriptions: {
+    path: "/subscriptions",
+    onConnect: async (connectionParams, webSocket) => {
+      console.log(
+        `Subscription client connected using Apollo server's built-in SubscriptionServer.`
+      );
+    }
+  }
+});
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
